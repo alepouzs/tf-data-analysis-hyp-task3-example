b47d37ee8b856617bb8550a82357ae7b5a976007
Yulia Izmailova <104653247+alepouzs@users.noreply.github.com>
2023-04-13 02:58:49 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,10 +1,10 @@
 import pandas as pd
 import numpy as np
-from statsmodels.stats.weightstats import ztest
+from scipy.stats import mannwhitneyu
 
 chat_id = 487382438 # Ваш chat ID, не меняйте название переменной
 
-def solution(x: np.array) -> bool: 
-    stat, p_value = ztest(x, value = 300, alternative = 's')
+def solution(x: np.array, y: np.array) -> bool: 
+    stat, p_value = mannwhitneyu(x, y, value = 500, alternative = 'smaller')
 
     return p_value < 0.06
