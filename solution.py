aef0d86e886ed2c4cf56a3c2e7de81fdb56c8c4e
Yulia Izmailova <104653247+alepouzs@users.noreply.github.com>
2023-04-13 03:19:53 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,10 +1,10 @@
 import pandas as pd
 import numpy as np
-from scipy.stats import mannwhitneyu
+from scipy.stats import ttest_ind
 
 chat_id = 487382438 # Ваш chat ID, не меняйте название переменной
 
 def solution(x: np.array, y: np.array) -> bool: 
-    stat, p_value = mannwhitneyu(x, y, alternative = 'less')
+    stat, p_value = ttest_ind(x, y, alternative = 'less')
 
     return p_value < 0.06
